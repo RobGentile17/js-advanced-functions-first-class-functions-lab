f044a7a327f042a50e32740795d0a3f39d8bae5d
Rob <robert.gentile@powerhrg.com>
2021-05-23 15:48:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+return drivers.slice(0,2) 
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(drivers.length - 2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareQuintupler) {
+return function() {
+return fareQuintupler * fareQuintupler
+}
+}
+function fareDoubler (doubleFare) {
+    return doubleFare * 2
+}
+function fareTripler (tripleFare) {
+return tripleFare * 3
+}
+function selectDifferentDrivers (drivers , jelena) {
+return jelena (drivers)
+}
